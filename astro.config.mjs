e58faaa5ff4a40f8ad132236d2f7ef9468f8dc53
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-16 13:27:20 +0100

    loads of little changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import mdx from "@astrojs/mdx";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
-  experimental: {
-    viewTransitions: true
-  },
-  output: "static",
+  output: "hybrid",
+  adapter: cloudflare()
 });
